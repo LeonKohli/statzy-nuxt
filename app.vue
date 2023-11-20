285d831ed6fae0ebceae54598ae2162b48bf6c49
Dreyex <mr.max.f@gmx.de>
2023-11-20 13:24:46 +0100

    Templates anlegen
    Co-authored-by: Magic-Burkhard <Magic-Burkhard@users.noreply.github.com>

--- app.vue
+++ app.vue
@@ -1,5 +1,47 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="main">
+    <h1> Welcome to Statzy </h1>
   </div>
 </template>
+
+<style>
+
+@media (prefers-color-scheme: dark) {
+  html 
+  {
+    background-color: black;
+    color: white;
+  }
+}
+
+@media (prefers-color-scheme: light) 
+{
+  html 
+  {
+    background-color: white;
+    color: black;
+  }
+}
+
+@media (prefers-color-scheme: no-preference)
+{
+  html {
+    background-color: black;
+    color: white;
+  }
+}
+
+div.main {
+  border: 1px solid red;
+  margin-left: 200px;
+  margin-right: 200px;
+  margin-top: 50px;
+  padding: 5px;
+}
+
+
+
+
+
+
+</style>
