417b85d74ebc9b54885cdfef74630cd6958326bf
SignumGG <lennard.lueben@web.de>
2023-11-20 14:04:35 +0100

    hole fachverfahren von db und gib an seite

---
+++ pages/fachverfahren.js
@@ -0,0 +1,24 @@
+const db = require('./database'); // Angenommen, 'database.js' ist Ihre Datenbankverbindungsdatei
+
+// Funktion, um die SQL-Abfrage auszuführen
+function getFachverfahren(tag) {
+    // Erstellen des SQL-Befehls
+    const query = "SELECT name, verf_id, tag, verwendungszweck, laufzeitverfahren, auftraggeber, verf_betreuung, kundenmanagement, fachadministration FROM fachverfahren WHERE tag ~* $1 ORDER BY name";
+    
+    // Ausführen der Abfrage
+    return db.query(query, [tag])
+        .then(result => {
+            // Verarbeitung und Rückgabe der Ergebnisse
+            return result.rows;
+        })
+        .catch(error => {
+            // Fehlerbehandlung
+            console.error('Fehler bei der Abfrage der Fachverfahren:', error);
+            throw error;
+        });
+}
+
+// Exportieren der Funktion, wenn sie in anderen Dateien verwendet werden soll
+module.exports = {
+    getFachverfahren
+};
