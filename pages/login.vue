df3e72d75b42f9c1d9e797ad7b9911ec84646770
LeonKohli <kohli.leon@gmail.com>
2023-11-21 11:50:50 +0100

    Remove 'required' attribute from remember checkbox

--- pages/login.vue
+++ pages/login.vue
@@ -21,7 +21,7 @@
             <div class="flex items-center justify-between">
               <div class="flex items-start">
                 <div class="flex items-center h-5">
-                  <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border rounded border-slate-600 bg-slate-500 focus:ring-3 focus:ring-blue-300" required />
+                  <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border rounded border-slate-600 bg-slate-500 focus:ring-3 focus:ring-blue-300" />
                 </div>
                 <div class="ml-3 text-sm">
                   <label for="remember" class="text-white">Remember me</label>
