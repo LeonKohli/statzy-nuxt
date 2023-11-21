201678c84478b0e5a4f3a6acbfa13296133d1e26
SignumGG <lennard.lueben@web.de>
2023-11-21 13:26:09 +0100

    funktionen für index endpoints

--- pages/functions.js
+++ pages/functions.js
@@ -13,4 +13,47 @@ export async function fetchFachverfahrenData() {
     }
   }
 
-  
+  export async function fetchPersonsData() {
+    try {
+      const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
+      const response = await fetch(`${baseUrl}/api/persons/index`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching persons data:', error);
+      return null; // Return null or handle the error as needed
+    }
+  }
+
+  export async function fetchServersData() {
+    try {
+      const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
+      const response = await fetch(`${baseUrl}/api/servers/index`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching servers data:', error);
+      return null; // Return null or handle the error as needed
+    }
+  }
+
+  export async function fetchAuswahlData() {
+    try {
+      const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
+      const response = await fetch(`${baseUrl}/api/servers/auswahl_data`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching Auswahl data:', error);
+      return null; // Return null or handle the error as needed
+    }
+  }
