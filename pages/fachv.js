3d25e3d8c541fb438ae387fb581780c3325a03c5
William Weber <webwilli004@gmail.com>
2023-11-21 11:34:16 +0100

    datenbankverbindung

--- pages/fachv.js
+++ pages/fachv.js
@@ -1,3 +1,17 @@
+import pg from 'pg';
+const { Pool } = pg;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+export const query = (text, params) => pool.query(text, params);
+
+
 // Beispielobjekt für die Datenbankverbindung
 const database = {
   // Funktion zum Abrufen aller Fachverfahren
