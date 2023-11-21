2f976522aa2eeb760adc29834aa0968b2e106613
Dreyex <mr.max.f@gmx.de>
2023-11-21 11:42:19 +0100

    fachverfahren styling + navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -4,14 +4,15 @@ import type { NuxtLink } from '#build/components';
 </script>
 
 <template>
-    <div class="rounded-lg mx-20 my-6 p-3 bg-slate-800 text-center font-bold">
+    <div class="rounded-lg mx-20 my-6 p-3 bg-slate-800 text-center font-bold" id="navbar">
          <span class="mx-6"> <NuxtLink to="/"> Statzy </NuxtLink> </span>
-         <span class="mx-6"> <NuxtLink to="/Fachverfahren"> Fachverfahren </NuxtLink> </span>
+         <span class="mx-6"> <NuxtLink to="/fachverfahren"> Fachverfahren </NuxtLink> </span>
+         <span class="mx-6"> <NuxtLink to="/login"> Login </NuxtLink> </span>
     </div>
 </template>
 
 <style>
-    div 
+    div#navbar 
     {
         color: white;
     }
--- pages/fachverfahren.vue
+++ pages/fachverfahren.vue
@@ -1,202 +1,154 @@
 <template>
-    <div>
-        <h1> API Endpoints </h1>
-
-        <!-- Create Server Form -->
-
+    <div> 
+        <h1> Fachverfahren </h1>
         <div>
-            <h2>Create Server</h2>
-
-            <form @submit.prevent="createServer">
-                <input v-model="newServerData.fachverfahren" placeholder="Fachverfahren" />
-
-                <input v-model="newServerData.name" placeholder="Name" />
-
-                <!-- Dropdown for 'Umgebung' -->
-
-                <select v-model="newServerData.umgebung">
-                    <option v-for="option in dropdownData.auswahl_umgebung" :value="option" :key="option">
-                        {{ option }}
-                    </option>
-                </select>
-
-                <input v-model="newServerData.laufzeit_server" placeholder="Laufzeit Server" />
-                <input v-model="newServerData.bereitstellungszeitpunkt" placeholder="Bereitstellungszeitpunkt" />
-                <input v-model="newServerData.verwendungszweck" placeholder="Verwendungszweck" />
-
-                <!-- Dropdown for 'Typ' -->
-
-                <select v-model="newServerData.typ">
-                    <option v-for="option in dropdownData.auswahl_typ" :value="option" :key="option">
-                        {{ option }}
-                    </option>
-                </select>
-
-                <!-- Dropdown for 'Netzwerk' -->
-
-                <select v-model="newServerData.netzwerk">
-                    <option v-for="option in dropdownData.auswahl_nic" :value="option" :key="option">
-                        {{ option }}
-                    </option>
-                </select>
-
-                <!-- Dropdown for 'RAM' -->
-
-                <select v-model="newServerData.ram">
-                    <option v-for="option in dropdownData.auswahl_ram" :value="option" :key="option">
-                        {{ option }}
-                    </option>
-                </select>
-
-                <!-- Dropdown for 'CPU' -->
-
-                <select v-model="newServerData.cpu">
-                    <option v-for="cpu in dropdownData.auswahl_cpu" :value="cpu" :key="cpu">
-                        {{ cpu }}
-                    </option>
-                </select>
-
-                <!-- Dropdown for 'OS' -->
-
-                <select v-model="newServerData.os">
-                    <option v-for="option in dropdownData.auswahl_os" :value="option" :key="option">
-                        {{ option }}
-                    </option>
-                </select>
-
-                <!-- Dropdown for 'Speichertyp' -->
-
-                <select v-model="newServerData.speichertyp">
-                    <option v-for="option in dropdownData.auswahl_speicher" :value="option" :key="option">
-                        {{ option }}
-                    </option>
-                </select>
-
-                <input v-model="newServerData.kapazität" placeholder="Kapazität" />
-
-                <!-- Dropdown for 'Erreichbarkeit' -->
-
-                <select v-model="newServerData.erreichbarkeit">
-                    <option v-for="option in dropdownData.auswahl_erreichbarkeit" :value="option" :key="option">
-                        {{ option }}
-                    </option>
-                </select>
-
-                <input v-model="newServerData.hochverfügbarkeit" placeholder="Hochverfügbarkeit" />
-
-                <input v-model="newServerData.vertraulichkeit" placeholder="Vertraulichkeit" />
-
-                <input v-model="newServerData.verfügbarkeit" placeholder="Verfügbarkeit" />
-                <input v-model="newServerData.integrität" placeholder="Integrität" />
-                <input v-model="newServerData.anmerkungen" placeholder="Anmerkungen" />
-                <input v-model="newServerData.user_ins" placeholder="User Ins" />
-                <input v-model="newServerData.user_upd" placeholder="User Upd" />
-
-                <input v-model="newServerData.associatedPersons" placeholder="Associated Persons" />
-
-                <button type="submit">Create Server</button>
-            </form>
-        </div>
-
-        <div>
-            <h2>List Servers</h2>
-            <button @click="listServers">Refresh List</button>
-            <ul>
-                <li v-for="server in servers" :key="server.server_id">
-                    {{ server.name }} ({{ server.fachverfahren }})
-                </li>
-            </ul>
+            <div id="suchmaske">
+                <h2> Verfahrens-Daten </h2>
+                <table id="verfahren">
+                    <tr>
+                        <td> <input placeholder="Verfahrens-ID"/> </td>
+                        <td> <input placeholder="Verfahrens-Name"/> </td>
+                        <td> <input placeholder="Tag"/> </td>
+                        <td> <input placeholder="Zweck"/> </td>
+                        <td> <input placeholder="Laufzeit"/> </td>
+                    </tr>
+                </table>
+                <br>
+                <h2> Personen-Daten </h2>
+                <table id="personen">
+                    <tr>
+                        <th>  </th>
+                        <th> Name </th>
+                        <th> Vorname </th>
+                        <th> Dezernat </th>
+                        <th> Telefon </th>
+                    </tr> 
+                    <tr>
+                        <th> Kundenmanagament: </th>
+                        <td> <input placeholder="Muster Name"/> </td>
+                        <td> "Muster Vorname" </td>
+                        <td> "Muster Dezernat" </td>
+                        <td> "Muster Telefon" </td>
+                    </tr>
+                    <tr>
+                        <th> Fachadministration: </th>
+                        <td> <input placeholder="Muster Name"/> </td>
+                        <td> "Muster Vorname" </td>
+                        <td> "Muster Dezernat" </td>
+                        <td> "Muster Telefon" </td>
+                    </tr>
+                    <tr>
+                        <th> Auftraggeber: </th>
+                        <td> <input placeholder="Muster Name"/> </td>
+                        <td> "Muster Vorname" </td>
+                        <td> "Muster Dezernat" </td>
+                        <td> "Muster Telefon" </td>
+                    </tr>
+                    <tr>
+                        <th> Verfahrensbetreuer: </th>
+                        <td> <input placeholder="Muster Name"/> </td>
+                        <td> "Muster Vorname" </td>
+                        <td> "Muster Dezernat" </td>
+                        <td> "Muster Telefon" </td>
+                    </tr>
+                </table>
+                <br>
+                <table id="systeme"> 
+                    <tr> 
+                        <td> Anzahl zugeordneter Systeme: "Anzahl" </td>
+                        <td> <input type="number" placeholder="Zahl"/> </td>
+                    </tr>
+                </table>
+                <button> Suchen </button>
+            </div>
+            <br>
+            <hr>
+            <br>
+            <div id="message">
+                <span> Message "Successfull | ERROR"</span>
+                <br>
+                <button> Erstellen </button>
+            </div>
         </div>
     </div>
+
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      newServerData: {
-        fachverfahren: '',
-        name: '',
-        umgebung: '',
-        laufzeit_server: '',
-        bereitstellungszeitpunkt: '',
-        verwendungszweck: '',
-        typ: '',
-        netzwerk: '',
-        ram: '',
-        cpu: '',
-        os: '',
-        speichertyp: '',
-        kapazität: '',
-        erreichbarkeit: '',
-        hochverfügbarkeit: '',
-        vertraulichkeit: '',
-        verfügbarkeit: '',
-        integrität: '',
-        anmerkungen: '',
-        user_ins: '',
-        user_upd: '',
-        associatedPersons: ''
-      },
-      servers: [],
-      dropdownData: {
-        umgebung: [],
-        typ: [],
-        netzwerk: [],
-        ram: [],
-        cpu: [],
-        os: [],
-        speichertyp: [],
-        erreichbarkeit: [],
-      }
-    };
-  },
-  mounted() {
-    if (process.client) {
-      this.fetchDropdownData();
-      this.listServers();
-    }
-  },
-  methods: {
-    async fetchDropdownData() {
-      try {
-        const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
-        const response = await fetch(`${baseUrl}/api/servers/auswahl_data`);
-        const data = await response.json();
-        this.dropdownData = data;
-      } catch (error) {
-        console.error('Error fetching dropdown data:', error);
-      }
-    },
-    async createServer() {
-      try {
-        const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
-        const response = await fetch(`${baseUrl}/api/servers/create.post`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(this.newServerData)
-        });
-        const responseData = await response.json();
-        console.log('Server Created:', responseData);
-        this.listServers(); // Refresh the list after creation
-      } catch (error) {
-        console.error('Error creating server:', error);
-      }
-    },
-    async listServers() {
-      try {
-        const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
-        const response = await fetch(`${baseUrl}/api/servers`);
-        const responseData = await response.json();
-        this.servers = responseData;
-      } catch (error) {
-        console.error('Error fetching servers:', error);
-      }
-    }
-  }
-};
+    const button = document.getElementsByTagName("button");
+
+    button.forEach(element => {
+        element.onClick = function() {
+            
+        }
+    });
 </script>
 
+<style>
+    table
+    {
+        text-align: center;
+        border-radius: 10px;
+        background-color: #1e293b;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    td, th
+    {
+        padding: 15px;
+    }
+
+    tr > th:nth-child(n+2):nth-child(-n+5)
+    {
+        font-size: 500;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        border-bottom: 1px solid #475569;
+    }
+
+    tr:nth-child(n+2):nth-child(-n+5) > th:first-child
+    {
+        border-right: 1px solid #475569;
+    }
+
+    input
+    {
+        text-align: center;
+    }
+
+    h1
+    {
+        font-size: xx-large;
+        font-weight: 700;
+        text-align: center;
+    }
+
+    h2
+    {
+        font-size: x-large;
+        text-align: center;
+        margin: 15px;
+    }
+
+    div#suchmaske
+    {
+        align-items: center;
+    }
+
+    button
+    {
+        width: 146px; 
+        height: 31px; 
+        background: #1E293B; 
+        border-radius: 9px;
+    }
+
+    button:active
+    {
+        scale: 80%;
+        background: #475569; 
+        border-radius: 9px
+    }
+</style>
 
