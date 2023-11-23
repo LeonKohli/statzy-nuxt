57943d5bf091ad9f0c4fcfc84c0b884cbbe7f87e
Magic-Burkhard <jacobhanke02@gmail.com>
2023-11-23 10:09:45 +0100

    Refactor login component

--- pages/login.vue
+++ pages/login.vue
@@ -54,18 +54,18 @@
 </template>
 
 <script>
-//  export default {
-//    data() {
-//      return {
-//        showPopup: false,
-//      };
-//    },
-//    methods: {
-//      handleLogin() {
-//        this.showPopup = true;
-//      },
-//    },
-//  };
+ export default {
+   data() {
+     return {
+       showPopup: false,
+     };
+   },
+   methods: {
+     handleLogin() {
+       this.showPopup = true;
+     },
+   },
+ };
 </script>
 
 <style>
@@ -205,25 +205,4 @@
         scale: 90%;
         transition-duration: 25ms;
     }
-@keyframes blob-bounce {
-  0% {
-    transform: translate(-100%, -100%) translate3d(0, 0, 0);
-  }
-
-  25% {
-    transform: translate(-100%, -100%) translate3d(100%, 0, 0);
-  }
-
-  50% {
-    transform: translate(-100%, -100%) translate3d(100%, 100%, 0);
-  }
-
-  75% {
-    transform: translate(-100%, -100%) translate3d(0, 100%, 0);
-  }
-
-  100% {
-    transform: translate(-100%, -100%) translate3d(0, 0, 0);
-  }
-}
 </style>
