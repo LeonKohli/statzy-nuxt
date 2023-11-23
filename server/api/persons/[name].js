7a6e7b4b639f591b4a3feca1b37f7089c927da99
LeonKohli <kohli.leon@gmail.com>
2023-11-23 08:34:42 +0100

    fixed person search api

--- server/api/persons/[name].js
+++ server/api/persons/[name].js
@@ -4,10 +4,14 @@ import { query } from '../../db.js';
 export default defineEventHandler(async (event) => {
   const name = event.context.params.name;
 
+  if (!name || name.length < 2) {
+    throw createError({ statusCode: 400, statusMessage: 'Input is too short' });
+  }
+
   let searchQuery = `
-    SELECT vorname, name, telefonnummer, dez 
+    SELECT vornam, name, telefonnummer, dez 
     FROM person
-    WHERE name LIKE $1`;
+    WHERE name LIKE $1 OR vornam LIKE $1`;
   
   const result = await query(searchQuery, [`%${name}%`]);
 
