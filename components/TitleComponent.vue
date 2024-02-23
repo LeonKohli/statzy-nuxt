ae56ae8ef635364fe59643c5f4c970b159d33d44
SignumGG <lennard.lueben@web.de>
2024-02-23 11:24:18 +0100

    titlecomponent change

--- components/TitleComponent.vue
+++ components/TitleComponent.vue
@@ -1,15 +1,9 @@
 <template>
   <div>
-    <h1 class="title-h1">{{ title }}</h1>
+    <h1 class="text-2xl font-bold text-center">{{ title }}</h1>
   </div>
 </template>
 
 <script setup>
 defineProps(['title']);
 </script>
-
-<style>
-.title-h1 {
-  @apply text-2xl font-bold text-center;
-}
-</style>
