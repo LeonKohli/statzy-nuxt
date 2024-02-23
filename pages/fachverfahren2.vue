4f00266a1e715bd090b7c80185f6711f93a020cb
LeonKohli <kohli.leon@gmail.com>
2024-02-23 11:36:09 +0100

    fix: unused imports

--- pages/fachverfahren2.vue
+++ pages/fachverfahren2.vue
@@ -9,10 +9,5 @@
   </template>
   
   <script setup>
-  import TitleComponent from '../components/TitleComponent.vue';
-  import SearchMaskComponent from '../components/SearchMaskComponent.vue';
-  import MessageContainerComponent from '../components/MessageContainerComponent.vue';
-  import ServerComponent from '../components/ServerComponent.vue';
-  import DatabaseComponent from '../components/DatabaseComponent.vue';
   </script>
   
--- pages/passwortVergessen.vue
+++ pages/passwortVergessen.vue
@@ -4,13 +4,13 @@
             <div class="flex justify-center mt-4">
                 <img src="/Statzy-Logo.png" class="w-32 md:w-48" alt="Statzy Logo" />
             </div>
-            <div class="w-full max-w-md p-8 bg-slate-800 rounded-xl shadow-lg md:mt-0 xl:p-10 dark:bg-gray-700">
+            <div class="w-full max-w-md p-8 shadow-lg bg-slate-800 rounded-xl md:mt-0 xl:p-10 dark:bg-gray-700">
                 <div class="space-y-6">
                     <h1 class="text-3xl font-bold leading-tight text-white md:text-4xl">Haben Sie Ihr Passwort vergessen?</h1>
                     <p class="text-white">Melden Sie sich beim Administrator.</p>
                     <p class="text-white">Nummer: BLA-BLA-BLA</p>
-                    <div class="mt-4 flex justify-center">
-                        <router-link to="/login" tag="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <div class="flex justify-center mt-4">
+                        <router-link to="/login" class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
                             Zurück zum Login
                         </router-link>
                     </div>
@@ -19,3 +19,9 @@
         </div>
     </section>
 </template>
+
+<script setup>
+</script>
+
+<style>
+</style>
